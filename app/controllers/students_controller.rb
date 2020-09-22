765d0b9af88b3cede0e1d416e1e2417ba775b234
lsuman83 <root@DESKTOP-3EM91F2.localdomain>
2020-09-22 13:07:54 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,14 @@
 class StudentsController < ApplicationController
   def index
     @students = Student.all
+    render "index"
   end
+
+  def show
+
+    @students = Student.find(params[:id])
+    render "show"
+
+  end
+
 end
